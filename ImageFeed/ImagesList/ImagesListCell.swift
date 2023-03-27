54c36d682860da94a4fc3d01c22d81a74cab5bcd
Margarita <pitinovamargo@gmail.com>
2023-03-27 23:36:13 +0200

    Сделала базовую структуру проекта
    Пока что не работает лайк

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Margarita Pitinova on 27.03.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Margarita Pitinova on 20.03.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func configCell(for cell: ImagesListCell) { }
+    
+}
+
+
+extension ImagesListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+                
+                guard let imageListCell = cell as? ImagesListCell else {
+                    return UITableViewCell()
+                }
+                
+                configCell(for: imageListCell)
+                return imageListCell
+    }
+    
+    
+}
