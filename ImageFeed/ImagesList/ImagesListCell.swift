38091b0c37d3aad9a51913d80b67da71d28db0a1
Margarita <pitinovamargo@gmail.com>
2023-04-13 00:16:47 +0200

    Завершение 8 спринта

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -8,5 +8,8 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
     static let reuseIdentifier = "ImagesListCell"
 }
