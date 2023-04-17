dbdcfa285392f9294f304df0e7cf4220084b9843
Margarita <pitinovamargo@gmail.com>
2023-04-17 21:56:51 +0200

    Создан экран профиля пользователя, добавлены констрейнты

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Margarita Pitinova on 13.04.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+}
