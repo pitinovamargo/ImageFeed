5fbbbe8be8d1510738c8705a2327bd782c5c4df2
Margarita <pitinovamargo@gmail.com>
2023-05-15 23:38:58 +0200

    Добавлен TabBar

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Margarita Pitinova on 13.04.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet private weak var avatar: UIImageView!
+    @IBOutlet private weak var nikname: UILabel!
+    @IBOutlet private weak var userName: UILabel!
+    @IBOutlet private weak var profileDescription: UILabel!
+    
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func logout(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+}
