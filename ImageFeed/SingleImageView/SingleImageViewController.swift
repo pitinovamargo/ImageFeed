5d3b3844ed5ac15ff6d7acca787de769c335139b
Margarita <pitinovamargo@gmail.com>
2023-05-16 15:24:41 +0200

    Добавлен метод для отображения верной картинки по нажатию на нее в ленте

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
-    
+    var image: UIImage!
+
     @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            imageView.image = image
+        }
 }
