8e4f01af8af6399f9c2eccc55a5a17f23da90c36
Margarita <pitinovamargo@gmail.com>
2023-05-16 14:06:46 +0200

    Сделан переход от таблицы к SingleImageView

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Margarita Pitinova on 16.05.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
