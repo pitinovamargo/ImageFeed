33a34df2952eb0be6db3d4710ee9d1e13e1c5559
Margarita <pitinovamargo@gmail.com>
2023-05-23 13:49:24 +0200

    Добавлены константы с ключами, доступами, URI и URL для Unsplash

---
+++ Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Margarita Pitinova on 23.05.2023.
+//
+
+import Foundation
+
+let accessKey = "5damU5G0-RGi6CU4KhEzI8_i7VSBJa3wEYGxz1rnl3k"
+let secretKey = "lptYM12dkL3rU_-DW9DOXHm9JOhROw4xVvfZ4ZkCybk"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseURL = URL(string: "https://api.unsplash.com")
