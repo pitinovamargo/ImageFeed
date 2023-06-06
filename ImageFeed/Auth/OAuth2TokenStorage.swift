a9ca36ec98696f2e90bcaec5488241f1b304b763
Margarita <pitinovamargo@gmail.com>
2023-06-06 16:13:46 +0200

    Реализовано сохранение токена в Keychain вместо UserDefaults

--- ImageFeed/Auth/OAuth2TokenStorage.swift
+++ ImageFeed/Auth/OAuth2TokenStorage.swift
@@ -6,16 +6,30 @@
 //
 
 import Foundation
+import SwiftKeychainWrapper
 
 class OAuth2TokenStorage {
     
     var token: String? {
         get {
-            return UserDefaults.standard.string(forKey: "accessToken")
+            return KeychainWrapper.standard.string(forKey: "accessToken")
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "accessToken")
+            if let newValue = newValue {
+                do {
+                    let isSuccess = KeychainWrapper.standard.set(newValue, forKey: "accessToken")
+                    guard isSuccess else {
+                        throw KeychainError.saveFailed
+                    }
+                } catch {
+                    print("Ошибка сохранения в Keychain: \(error)")
+                }
+            }
         }
     }
+}
+
+enum KeychainError: Error {
+    case saveFailed
 }
 
